c676c19820673ff6f89bec1225675b5bd7bc639b
jmccormac01 <jmccormac001@gmail.com>
2015-12-01 00:24:03 +0000

    updating the checkTestModule script

--- checkTestModule.py
+++ checkTestModule.py
@@ -1,5 +1,6 @@
 # ensure name sever is running with:
 # python -m Pyro4.naming -n 10.2.5.32
+# ensure the centralHub is running too
 import Pyro4
-ts=Pyro4.Proxy("PYRONAME:example.test")
+ts=Pyro4.Proxy("PYRONAME:central.hub")
 print ts.running()
