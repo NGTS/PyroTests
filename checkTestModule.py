8823df789f9651fec4b8fec9e4bf1ad3023ecdd6
Simon Walker <s.r.walker101@googlemail.com>
2016-03-03 13:34:22 -0300

    Update checkTestModule to test new api

--- checkTestModule.py
+++ checkTestModule.py
@@ -1,6 +1,7 @@
 # ensure name sever is running with:
 # python -m Pyro4.naming -n 10.2.5.32
 # ensure the centralHub is running too
+from __future__ import print_function
 import Pyro4
 ts=Pyro4.Proxy("PYRONAME:central.hub")
-print ts.running()
+print(ts.sleeptime)
